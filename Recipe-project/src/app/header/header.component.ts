c2118f4f46444cfa9ffc55f0fd12eebcb1874eae
AbasBashir <MrAbasBashir@gmail.com>
2024-03-27 13:52:23 +0100

    Changed angular folder structure

---
+++ Recipe-project/src/app/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent{
+  loginError: string = "";
+
+  constructor(public authService: AuthService, private router: Router) { }
+
+
+  logout(): void {
+    this.authService.logout().subscribe(success => {
+      if (success) {
+        this.router.navigate(['/login']);
+      } else {
+        // Display message in case of login failure
+        this.loginError = "Unable to logout";
+      }
+    });
+  }
+
+}
---
+++ Recipe-project/src/app/services/recipes.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private key: string = 'b3b2f3bbbeb84eb08d3ef179d355d541';
+  private BASE_URL: string = 'https://api.spoonacular.com/recipes';
+
+
+  constructor(private http: HttpClient) { }
+
+  fetchRecipes(): Observable<any>{ 
+    return this.http.get(`${this.BASE_URL}/complexSearch?number=100&apiKey=${this.key}`);
+  }
+
+  fetchSpecificRecipe(id: string): Observable<any>{
+    return this.http.get(`${this.BASE_URL}/${id}/information?apiKey=${this.key}`);
+  }
+
+  fetchMealOption(id: string): Observable<any>{ 
+    return this.http.get(`${this.BASE_URL}/complexSearch?type=${id}&apiKey=${this.key}`);
+  }
+
+  fetchMealByAllergy(checkedAllergies: string[]): Observable<any>{ 
+    const intolerances = checkedAllergies.join(',');
+    return this.http.get(`${this.BASE_URL}/complexSearch?intolerances=${intolerances}&number=100&apiKey=${this.key}`);
+  } 
+
+  fetchMealByDiet(checkedDiets: string[]): Observable<any>{ 
+    const diets = checkedDiets.join(',');
+    return this.http.get(`${this.BASE_URL}/complexSearch?diet=${diets}&number=100&apiKey=${this.key}`);
+  } 
+
+  advancedFilter(params: string): Observable<any>{ 
+    return this.http.get(`${this.BASE_URL}/complexSearch?${params}&number=100&apiKey=${this.key}`);
+  }
+}
+
